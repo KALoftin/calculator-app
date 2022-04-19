409aa8e720f2b3afd4bbbd4028406280ddcc9c5d
Kyle Loftin <kaloftin85@gmail.com>
2022-04-19 15:57:53 -0700

    initial setup

---
+++ myscript.js
@@ -0,0 +1,24 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(operator, x, y) {
+  console.log(operator(x, y));
+}
+
+operate(add, 5, 10);
+operate(subtract, 10, 5);
+operate(multiply, 10, 5);
+operate(divide, 10, 5);
