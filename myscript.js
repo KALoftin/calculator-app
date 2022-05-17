acbba97113196279018c70fdc1b2cbf78d036f33
Kyle Loftin <kaloftin85@gmail.com>
2022-05-17 15:39:47 -0700

    Fixed edge cases with equals sign and added decimal button functionality

--- myscript.js
+++ myscript.js
@@ -1,13 +1,23 @@
 const input = document.getElementById('calc-input');
 const equals = document.getElementById('btn-equal');
 const clear = document.getElementById('btn-clear');
+const decimal = document.getElementById('btn-dec');
 let displayValue = '';
-let firstNumInput;
+let firstNumInput = 0;
 let inputOperator = '';
 let newOpNums;
 let didUserInputNums = false;
 
 input.value = '0';
+
+function clearAll() {
+  displayValue = '';
+  firstNumInput = 0;
+  inputOperator = '';
+  didUserInputNums = false;
+  decimal.disabled = false;
+  input.value = '0';
+}
 
 function operate(operator, x, y) {
   if (operator === '/') {
@@ -23,6 +33,15 @@ function operate(operator, x, y) {
   if (operator === '-') return x - y;
   if (operator === '+') return x + y;
 }
+
+decimal.addEventListener('click', function () {
+  // Once dec button is clicked, check if there's already a
+  // decimal displayed. If not then add the decimal, if there
+  // is, do nothing
+  displayValue += decimal.value;
+  input.value = displayValue;
+  decimal.disabled = true;
+});
 
 // Creates an array from the number buttons in the DOM
 // and adds an event listener to each
@@ -50,42 +69,50 @@ Array.from(document.getElementsByClassName('oper-btns')).forEach(function (e) {
       displayValue = '';
       input.value = firstNumInput;
       inputOperator = btnID.value;
+      decimal.disabled = false;
     } else {
       // Add the first input numbers and operator to memory
       if (didUserInputNums) {
         inputOperator = btnID.value;
         firstNumInput = Number(displayValue);
         displayValue = '';
-        input.value = displayValue;
+        input.value = firstNumInput;
+        decimal.disabled = false;
         didUserInputNums = false;
         // Update calculation call if first input number is already there
       } else if (firstNumInput || firstNumInput === 0) {
         inputOperator = btnID.value;
         displayValue = '';
-        input.value = displayValue;
+        decimal.disabled = false;
+        input.value = firstNumInput;
         // Alerts user to start with a number not an operator
       } else {
         window.alert('Start with a number not operator');
-        displayValue = '';
+        clearAll();
       }
     }
   });
 });
 
 equals.addEventListener('click', function () {
-  const newInputNums = Number(displayValue);
-  const newNum = operate(inputOperator, firstNumInput, newInputNums);
-  input.value = newNum;
-  firstNumInput = newNum;
-  displayValue = '';
-  inputOperator = '';
-  didUserInputNums = false;
+  if (didUserInputNums && inputOperator === '') {
+    displayValue = '';
+    decimal.disabled = false;
+  } else if (!didUserInputNums) {
+    const newNum = operate(inputOperator, firstNumInput, Number(input.value));
+    input.value = newNum;
+    displayValue = '';
+    decimal.disabled = false;
+  } else {
+    const newInputNums = Number(displayValue);
+    const newNum = operate(inputOperator, firstNumInput, newInputNums);
+    input.value = newNum;
+    firstNumInput = newNum;
+    displayValue = '';
+    inputOperator = '';
+    didUserInputNums = false;
+    decimal.disabled = false;
+  }
 });
 
-clear.addEventListener('click', function () {
-  input.value = '0';
-  displayValue = '';
-  inputOperator = '';
-  didUserInputNums = false;
-  firstNumInput = NaN;
-});
+clear.addEventListener('click', clearAll);
